53ef1b558a77682c05dcbd315b09449c3bba3bdc
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-07-29 19:51:39 +0900

    やっぱり backend は必要っぽい

---
+++ lib/CCSessions.pm
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+package CCSessions;
+use File::AddInc qw($libDir);
+
+use MOP4Import::Base::CLI_JSON -as_base
+  , [fields =>
+     [claude_projects => default => "$ENV{HOME}/.claude/projects"],
+     [cache_dir => default => "$libDir/../var/tmp"],
+     qw(
+       _cache
+     )
+   ];
+
+sub projects {
+  (my MY $self) = @_;
+  map {
+    File::Basename::basename($_)
+  }
+  glob "$self->{claude_projects}/-*";
+}
+
+sub scan {
+  (my MY $self, my $fn) = @_;
+}
+
+MY->cli_run(\@ARGV) unless caller;
+
+1;

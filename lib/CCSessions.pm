32191125965da1a856a15ac2478ad602ba244808
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-07-29 20:40:28 +0900

    しょーもないミスをしている感じがする

--- lib/CCSessions.pm
+++ lib/CCSessions.pm
@@ -7,24 +7,67 @@ use MOP4Import::Base::CLI_JSON -as_base
      [claude_projects => default => "$ENV{HOME}/.claude/projects"],
      [cache_dir => default => "$libDir/../var/tmp"],
      qw(
-       _cache
+       _id_cache
+       _session_cache
      )
    ];
 
 use File::stat;
 
-sub sessions {
+use MOP4Import::Types
+  (FileInfo => [[fields => qw(id dir stat)]],
+   # FileCache => [[fields => qw(mtime lines)]],
+  );
+
+sub read_session_item {
+  (my MY $self, my ($id, $ix)) = @_;
+  
+}
+
+sub scan_session {
+  (my MY $self, my ($id)) = @_;
+  my $fn = $self->session_filepath($id);
+
+  $self->{_session_cache}{$id} //= do {
+    open my $fh, '<', $fn or Carp::croak "no such file: $fn";
+    my @result;
+    my $fpos = 0;
+    while (my $json = <$fh>) {
+      push @result, $fpos;
+      $fpos = tell $fh;
+    }
+    \@result;
+  };
+}
+
+sub session_filepath {
+  (my MY $self, my ($id)) = @_;
+  my FileInfo $rec = $self->session_fileinfo($id);
+  "$self->{claude_projects}/$rec->{dir}/$id.jsonl";
+}
+
+sub session_fileinfo {
+  (my MY $self, my ($id)) = @_;
+  my FileInfo $rec = $self->{_id_cache}{$id}
+    or Carp::croak "No such session: $id";
+}
+
+sub session_list {
   (my MY $self, my $project) = @_;
   map {
-    +{
-      dir => $project, fn => File::Basename::basename($_),
-      stat => stat($_),
-    }
+    my $id = File::Basename::basename($_);
+    $id =~ s/\.jsonl\z//;
+    my FileInfo $rec = +{};
+    $rec->{dir} = $project;
+    $rec->{id} = $id;
+    $rec->{stat} = stat($_);
+    $self->{_id_cache}{$id} = $rec;
+    $rec;
   }
   glob "$self->{claude_projects}/$project/*.jsonl";
 }
 
-sub projects {
+sub project_list {
   (my MY $self) = @_;
   map {
     File::Basename::basename($_)
